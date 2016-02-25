209b5b58c7d5d6de2fcf155f4669eea472923341
lucasbegnini <lucasbegnini@gmail.com>
2016-02-25 10:17:46 -0300

    [ExitMenu] Done

--- padraoOcean/Assets/Scripts/Patterns/ExitEsc.cs
+++ padraoOcean/Assets/Scripts/Patterns/ExitEsc.cs
@@ -1,15 +1,30 @@
 ﻿using UnityEngine;
 using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class ExitEsc : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
+    [SerializeField]
+    private string scenetoGo;
+    [SerializeField]
+    private GameObject ExitConfirmPanel;
+
+    void Start()
+    {
+        if(SceneManager.GetActiveScene().name.Equals("Menu"))
+            ExitConfirmPanel.SetActive(false);
+    }
 	void Update () {
-	
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (SceneManager.GetActiveScene().name.Equals("Menu"))
+            {
+                ExitConfirmPanel.SetActive(true);
+            }
+            else {
+                SceneManager.LoadScene(scenetoGo);
+            }
+        }
 	}
 }
